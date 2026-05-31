bfbe4e34c35ff953e4ce46f98bee00181f9dcc67
unknown <you@example.com>
2026-05-31 23:43:04 +0530

    run

--- src/main/java/baseLibrary/BaseClassLibrary.java
+++ src/main/java/baseLibrary/BaseClassLibrary.java
@@ -36,7 +36,7 @@ public class BaseClassLibrary implements ExcelUtility ,ApplicationUtility
     @AfterTest
      public void closedBrowser()
      {
-    	 //driver.quit();
+    	 driver.close();
      }
 
 	 @Override
